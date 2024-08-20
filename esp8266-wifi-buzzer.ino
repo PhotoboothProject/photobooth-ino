b70eb4707cf9a593ac41186863690b605fc17ee4
Andreas Skomski <andi@unlegacy-android.org>
2024-08-20 07:59:31 +0200

    Uesp8266-wifi-buzzer.ino: add const for Remotebuzzer-Server

--- esp8266-wifi-buzzer.ino
+++ esp8266-wifi-buzzer.ino
@@ -9,23 +9,28 @@
 
 #if SOFTWARE_SERIAL_AVAILABLE
 #include <SoftwareSerial.h>
-
 #endif
 
 // PIN definition
 OneButton button(D1, true);
 
 // WIFI Connection
-const char * ssid = "SSID";
-const char * password = "WIFI Password";
-
-HTTPClient http;
-WiFiClient client;
+const char * ssid = "Your SSID";
+const char * password = "Your WIFI Password";
 
 // Define WIFI-IP and Gateway
 WiFiServer server(80);
-IPAddress ip(xx, xx, xx, xx);
-IPAddress gateway(xx, xx, xx, xx);
+// Static IP of ESP8266
+IPAddress ip(192, 168, 1, 100);
+// Gateway (typically your router)
+IPAddress gateway(192, 168, 1, 1);
+// Photobooth remotebuzzer server IP
+const char *photoboothIP = "192.168.1.50";
+// Photobooth remotebuzzer server port
+const int photoboothPort = 14711;
+
+WiFiClient client;
+HTTPClient http;
 
 void setup(void) {
   delay(500);
@@ -49,7 +54,7 @@ void setup(void) {
   Serial.println("");
   Serial.println("WiFi connected");
 
-  // Starte den server
+  // Start the server
   server.begin();
   Serial.println("Server started");
 
@@ -59,7 +64,7 @@ void setup(void) {
   Serial.print(WiFi.localIP());
   Serial.println("/");
 
-  Serial.println(F("GET Request to Buzzer-Remote-Server ready!"));
+  Serial.println(F("GET Request to Remotebuzzer-Server ready!"));
   Serial.println(F("---------------------------------------"));
 
   /* Button Click Event */
@@ -75,14 +80,12 @@ void loop(void) {
 void singleclick() {
   Serial.println(F("GET request for single picture"));
 
-  HTTPClient http;
-  // http://[Photobooth IP]:[Port]/commands/start-picture
-  http.begin(client, "http://xx.xx.xx.xx:14711/commands/start-picture");
+  String url = String("http://") + photoboothIP + ":" + photoboothPort + "/commands/start-picture";
+  http.begin(client, url);
 
   int httpCode = http.GET();
 
   if (httpCode > 0) {
-
     String payload = http.getString();
     Serial.println(httpCode);
     Serial.println(payload);
@@ -94,15 +97,13 @@ void singleclick() {
 }
 
 void longclick() {
-
   Serial.println(F("GET Request for Collage"));
 
-  HTTPClient http;
-
-  // http://[Photobooth IP]:[Port]/commands/start-collage
-  http.begin(client, "http://xx.xx.xx.xx:14711/commands/start-collage");
+  String url = String("http://") + photoboothIP + ":" + photoboothPort + "/commands/start-collage";
+  http.begin(client, url);
 
   int httpCode = http.GET();
+
   if (httpCode > 0) {
     String payload = http.getString();
     Serial.println(httpCode);
