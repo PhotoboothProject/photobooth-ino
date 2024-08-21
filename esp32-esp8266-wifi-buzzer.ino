2f26b328c5278cb58bc6f2626d0b03150048611d
Andreas Skomski <andi@unlegacy-android.org>
2024-08-21 06:28:57 +0200

    Update and rename esp8266-wifi-buzzer.ino to esp32-esp8266-wifi-buzzer.ino

--- esp32-esp8266-wifi-buzzer.ino
+++ esp32-esp8266-wifi-buzzer.ino
@@ -4,9 +4,22 @@
  */
 
 #include <OneButton.h>
-#include <ESP8266WiFi.h>
-#include <ESP8266HTTPClient.h>
 
+// Define whether you are using ESP32
+const bool isESP32 = true;
+
+#if isESP32
+// ESP32 includes
+#include <WiFi.h>
+#include <WebServer.h>
+
+#else
+// ESP8266 includes
+#include <ESP8266WiFi.h>
+#include <WiFiClient.h>
+#include <ESP8266WebServer.h>
+
+#endif
 #if SOFTWARE_SERIAL_AVAILABLE
 #include <SoftwareSerial.h>
 #endif
@@ -20,7 +33,7 @@ const char * password = "Your WIFI Password";
 
 // Define WIFI-IP and Gateway
 WiFiServer server(80);
-// Static IP of ESP8266
+// Static IP of ESP8266 or ESP32
 IPAddress ip(192, 168, 1, 100);
 // Gateway (typically your router)
 IPAddress gateway(192, 168, 1, 1);
@@ -38,36 +51,49 @@ void setup(void) {
   Serial.print(F("Setting up static IP: "));
   Serial.println(ip);
 
-  // Connect WIFI
+  // Connect to Wi-Fi
   Serial.println();
   Serial.println();
   Serial.print("Connecting to ");
   Serial.println(ssid);
-  IPAddress subnet(255, 255, 255, 0);
-  WiFi.config(ip, gateway, subnet);
+
+  WiFi.mode(WIFI_STA);
+  WiFi.config(ip, gateway, IPAddress(255, 255, 255, 0));
   WiFi.begin(ssid, password);
 
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
     Serial.print(".");
   }
+
+  // Print connection details
   Serial.println("");
   Serial.println("WiFi connected");
-
-  // Start the server
-  server.begin();
-  Serial.println("Server started");
-
-  // Show IP Address inside Serial Monitor
   Serial.print("Use this URL : ");
   Serial.print("http://");
   Serial.print(WiFi.localIP());
   Serial.println("/");
 
+  #if isESP32
+    // Disable Wi-Fi sleep on ESP32
+    esp_wifi_set_ps(WIFI_PS_NONE);
+  #else
+    // Disable Wi-Fi sleep on ESP8266
+    WiFi.setSleep(false);
+  #endif
+
+  // Enable auto-reconnect and persistent settings
+  WiFi.setAutoReconnect(true);
+  WiFi.persistent(true);
+
+  // Start the server
+  server.begin();
+  Serial.println("Server started");
+
   Serial.println(F("GET Request to Remotebuzzer-Server ready!"));
   Serial.println(F("---------------------------------------"));
 
-  /* Button Click Event */
+  // Button click events
   button.attachLongPressStop(longclick);
   button.attachClick(singleclick);
 }
