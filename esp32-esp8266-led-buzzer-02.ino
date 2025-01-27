2b6ed5a58b84061870b12453a4612c30ad922475
Andreas Skomski <andi@unlegacy-android.org>
2025-01-27 19:07:25 +0100

    Update esp32-esp8266-led-buzzer-02.ino

--- esp32-esp8266-led-buzzer-02.ino
+++ esp32-esp8266-led-buzzer-02.ino
@@ -43,7 +43,7 @@ const char * ssid = "<Your-SSID>";
 const char * password = "<Your-Password>";
 
 // ADD NETWORK SETTINGS
-WiFiServer server(80);
+WebServer server(80);
 // Static IP of ESP8266 or ESP32
 IPAddress ip(192, 168, 1, 100);
 // Gateway (typically your router)
