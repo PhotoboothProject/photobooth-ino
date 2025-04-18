c720893d9236cc7dad0021eb4661c79ff26c32d9
Andreas Skomski <andi@unlegacy-android.org>
2025-04-18 20:03:06 +0200

    Update esp32-esp8266-wifi-buzzer.ino

--- esp32-esp8266-wifi-buzzer.ino
+++ esp32-esp8266-wifi-buzzer.ino
@@ -168,7 +168,7 @@ void collageAction() {
 }
 
 void printAction() {
-  Serial.println(F("GET Request for Print");
+  Serial.println(F("GET Request for Print"));
 
   String url = String("http://") + photoboothIP + ":" + photoboothPort + "/commands/start-print";
   http.begin(client, url);
